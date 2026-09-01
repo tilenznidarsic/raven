c5e53b952464e8c1bf8055dc25f10c8336b13527
TilenZnidarsic <tilen.x360@gmail.com>
2026-09-01 17:57:25 +0200

    Adjust home page bg color

--- packages/web/src/pages/HomePage.tsx
+++ packages/web/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-slate-950 text-slate-100">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gray-900 text-gray-100">
       <h1 className="text-4xl font-bold tracking-tight">Home</h1>
     </div>
   );
